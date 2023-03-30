08230a11d5597e7cf0edda734226cff46fad2b77
Carlos Loaiza <carlos.loaiza@auplex.mx>
2023-03-30 09:36:07 -0700

    creación de la base de datos

---
+++ ordenes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `orden` (
+    `id` int(11) NOT NULL,
+    `fecha_orden` datetime NOT NULL,
+    `id_cte` int(10) NOT NULL,
+    `id_pdto` int(10) NOT NULL,
+    `cantidad` float(5, 2) NOT NULL,
+    `creado` datetime NOT NULL
+);
